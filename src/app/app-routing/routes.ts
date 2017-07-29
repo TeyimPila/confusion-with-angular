82a17d177f6e51442dded91337237fbf2058ffc7
Teyim Pila <teyim.pila@aun.edu.ng>
2017-07-29 15:06:12 +0100

    setup routing using the basics of angular router

---
+++ src/app/app-routing/routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+// Components whose routes need to be created
+import { MenuComponent } from '../menu/menu.component';
+import { DishdetailsComponent } from '../dishdetails/dishdetails.component';
+import { HomeComponent } from '../home/home.component';
+import { AboutComponent } from '../about/about.component';
+import { ContactComponent } from '../contact/contact.component'
+
+
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'menu', component: MenuComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+]
