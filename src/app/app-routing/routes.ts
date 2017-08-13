ea856dce7e7bd73c26df740e71ef77ac6e706fc9
Teyim Pila <teyim.pila@aun.edu.ng>
2017-08-13 08:15:06 +0100

    create the promotion schema and implement contact us component

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -5,13 +5,13 @@ import { MenuComponent } from '../menu/menu.component';
 import { DishdetailsComponent } from '../dishdetails/dishdetails.component';
 import { HomeComponent } from '../home/home.component';
 import { AboutComponent } from '../about/about.component';
-import { ContactComponent } from '../contact/contact.component'
+import { ContactComponent } from '../contact/contact.component';
 
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'menu', component: MenuComponent},
     {path: 'about', component: AboutComponent},
-    {path: 'contact', component: ContactComponent},
+    {path: 'contactus', component: ContactComponent},
     {path: '', redirectTo: '/home', pathMatch: 'full'},
-]
+];
---
+++ src/app/shared/promotion.ts
@@ -0,0 +1,9 @@
+export class Promorion {
+    id: number;
+    name: string;
+    image: string;
+    label: string;
+    price: string;
+    featured: boolean;
+    description: string;
+}
